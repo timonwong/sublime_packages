9e6abb1812410f9c6eb29c87d8c19cedf5c5ef5e
Timon Wong <timon86.wang@gmail.com>
2012-11-01 22:42:15 +0800

    Refactored generator

--- build.py
+++ build.py
@@ -6,22 +6,19 @@ import json
 from string import Template
 
 
-# ==== CONFIG ====
-g_packages = ["SublimeAStyleFormatter", "SublimeAlternate", "OmniMarkupPreviewer"]
-
-
-# Read every package.json file
+# Get every package.json file from packages
 def get_package_files(packages):
     system = platform.system()
-    if system == "Windows":
-        st2_packages_dir = os.path.join(os.environ["APPDATA"], "Sublime Text 2\\Packages")
-    elif system == "Darwin":
-        st2_packages_dir = "~/Library/Application Support/Sublime Text 2/Packages"
+    if system == 'Windows':
+        st2_packages_dir = os.path.expandvars('${APPDATA}\\Sublime Text 2\\Packages')
+    elif system == 'Darwin':
+        st2_packages_dir = '~/Library/Application Support/Sublime Text 2/Packages'
     else:
-        st2_packages_dir = "~/.config/sublime-text-2/Packages"
-    package_files = []
-    for package in packages:
-        package_files.append(os.path.join(st2_packages_dir, package, "package.json"))
+        st2_packages_dir = '~/.config/sublime-text-2/Packages'
+    package_files = [
+        os.path.join(st2_packages_dir, package, 'package.json')
+        for package in packages
+    ]
     return package_files
 
 
@@ -31,54 +28,55 @@ def shell_execute(args, working_dir):
 
 def get_package_git_information(package_file):
     package_dir = os.path.dirname(package_file)
-    # Get version
-    proc_git_tag = shell_execute(['git', 'tag'], package_dir)
-    latest_tags = proc_git_tag.communicate()[0].strip()
-    latest_tags = latest_tags.split('\n')
-    # Get the newest
-    latest_tag = latest_tags[-1]
-    latest_version = latest_tag[1:]
+    # Get the latest version information, base on the tag
+    proc_git_tag = shell_execute(['git', 'describe', '--abbrev=0', '--tags'], package_dir)
+    last_tag = proc_git_tag.communicate()[0].strip()
+    # Assumes that the tag is following this format: v1.0.2
+    last_version = last_tag[1:]
     # Get date
     proc_git_log = shell_execute(
-        ['git', 'log', "--pretty=format:%ci", "-1", latest_tag], package_dir)
+        ['git', 'log', '--pretty=format:%ci', '-1', last_tag], package_dir)
     last_modified = proc_git_log.communicate()[0].strip()
     last_modified = last_modified[:19]
-    if (latest_tag == "" or latest_version == ""):
+    if (last_tag == "" or last_version == ""):
         raise Exception("No Git Version Information")
-    return latest_tag, latest_version, last_modified
+    return {
+        'last_tag': last_tag,
+        'last_version': last_version,
+        'last_modified': last_modified,
+    }
 
 
 def get_package_json_information(package_file):
     # Parsing package.json, get params
-    f = open(package_file, "r")
-    package_json = json.loads(f.read())
-    name = package_json["name"]
-    description = package_json["description"]
-    author = package_json["author"]
-    homepage = package_json["homepage"]
-    repo = package_json["repo"]
-    platforms = package_json["platforms"]
-    return {"name": name, \
-            "description": description, \
-            "author": author, \
-            "homepage": homepage, \
-            "repo": repo, \
-            "platforms": platforms}
+    with open(package_file, "r") as f:
+        package_json = json.loads(f.read())
+        name = package_json["name"]
+        description = package_json["description"]
+        author = package_json["author"]
+        homepage = package_json["homepage"]
+        repo = package_json["repo"]
+        platforms = package_json["platforms"]
+        return {"name": name, \
+                "description": description, \
+                "author": author, \
+                "homepage": homepage, \
+                "repo": repo, \
+                "platforms": platforms}
 
 
 def build_platforms_json_string(package_git_information, platforms, repo):
     template_platform = Template("""
                 "$platform": [
                     {
-                        "version": "$latest_version",
-                        "url": "https://nodeload.github.com/timonwong/$repo/zipball/$latest_tag"
+                        "version": "$last_version",
+                        "url": "https://nodeload.github.com/timonwong/$repo/zipball/$last_tag"
                     }
                 ]""")
-    latest_tag, latest_version, last_modified = package_git_information
-    json_platforms = []
-    for platform in platforms:
-        json_platforms.append(template_platform.substitute(platform=platform, latest_version=latest_version, \
-                                                           latest_tag=latest_tag, repo=repo))
+    json_platforms = [
+        template_platform.substitute(platform=platform, repo=repo, **package_git_information)
+        for platform in platforms
+    ]
     return ",".join(json_platforms)
 
 
@@ -92,7 +90,7 @@ def build_packages_json_file(packages):
             "author": "$author",
             "homepage": "$homepage",
             "last_modified": "$last_modified",
-            "platforms": {$platforms
+            "platforms": {$platform_json_string
             }
         }""")
 
@@ -101,28 +99,43 @@ def build_packages_json_file(packages):
         try:
             package_json_info = get_package_json_information(package_file)
             package_git_info = get_package_git_information(package_file)
-            _, _, last_modified = package_git_info
-            platforms = build_platforms_json_string(package_git_info, \
-                                                    package_json_info["platforms"], \
-                                                    package_json_info["repo"])
-            packages_json.append(template_main.substitute(name=package_json_info["name"], \
-                                                          description=package_json_info["description"], \
-                                                          author=package_json_info["author"], \
-                                                          homepage=package_json_info["homepage"], \
-                                                          last_modified=last_modified, \
-                                                          platforms=platforms))
+            platforms_json_string = build_platforms_json_string(
+                package_git_info,
+                package_json_info["platforms"],
+                package_json_info["repo"]
+            )
+            info = dict(package_json_info, **package_git_info)
+            packages_json.append(template_main.substitute(
+                platform_json_string=platforms_json_string, **info
+            ))
         except:
             import traceback
             traceback.print_exc()
     return ",".join(packages_json)
 
 if __name__ == "__main__":
-    f = open('packages.json', 'w')
-    f.write("""{
+    import sys
+    packages = None
+
+    with open('package_list.txt', 'r') as f:
+        packages = f.read().split()
+
+    if packages is None:
+        sys.stderr.write('No package defined in package_list.txt\n')
+        sys.exit(1)
+
+    with open('packages.json', 'w') as f:
+        f.write(
+"""\
+{
     "schema_version": "1.2",
-    "packages": [""")
-    f.write(build_packages_json_file(g_packages))
-    f.write("""
+    "packages": [\
+"""
+        )
+        f.write(build_packages_json_file(packages))
+        f.write(
+"""
     ]
-}""")
-    f.close()
+}\
+"""
+        )
